ea5d72479c1ba296b757efdfd795392d76eb3b13
astroysmr <astro.yoshimura@gmail.com>
2021-05-04 17:44:04 +0900

    initial update

--- b4rpipe/__init__.py
+++
@@ -1,8 +0,0 @@
-from b4rpipe.main import *
-
-__copyright__    = 'Copyright (C) 2021 B4R team'
-__version__      = '0.0.8'
-__author__       = 'astroysmr'
-__author_email__ = 'astro.yoshimura@gmail.com'
-__url__          = 'https://github.com/LMT-heterodyne-dev/b4rpipe'
-__all__ = []
